726e8efe5d216084bf8e12231e6f508827c4a2c3
Ariel Eitner <105615462+Ariel-Eitner@users.noreply.github.com>
2024-04-09 20:03:02 -0300

    port

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   dotenv.config();
 
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService); // Obtiene el servicio de configuración
+  const port = configService.get('PORT') || 3600;
   app.enableCors({
     origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
-  await app.listen(3600);
+  await app.listen(port);
 }
 bootstrap();
